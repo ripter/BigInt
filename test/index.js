ce4330ffddb7920f564235fda3420e0811f2be96
Chris Richards <ripter001@gmail.com>
2013-09-07 21:40:12 -0700

    added tests for numbers larger than 9007199254740992

--- test/index.js
+++ test/index.js
@@ -6,75 +6,98 @@ describe('BigInt basic Math', function() {
 	var num = null;
 	var result = null;
 
-	beforeEach(function() {
-		num = new BigInt(746)
-		result = null;
+
+	describe('normal INTs', function() {
+		beforeEach(function() {
+			num = new BigInt(746)
+			result = null;
+		});
+
+		it ('should initalize with a number', function() {
+			result = num.toString();
+
+			result.should.equal('746');
+		});
+
+		it ('should addOne under 9', function() {
+			num.addOne();
+			result = num.toString();
+
+			result.should.equal('747');
+		});
+
+		it ('should addOne at 9', function() {
+			num.addOne(); 	// 7
+			num.addOne(); 	// 8
+			num.addOne(); 	// 9
+			num.addOne(); 	// 10
+
+			result = num.toString();
+			result.should.equal('750');
+		});
+
+		it ('should handle 9 + 1', function() {
+			num = new BigInt(9);
+			num.addOne();
+
+			result = num.toString();
+			result.should.equal('10');
+		});
+
+		it ('should subtractOne', function() {
+			num.subtractOne();
+
+			result = num.toString();
+			result.should.equal('745');
+		});
+
+		it ('should roll backwars', function() {
+			num.subtractOne(); // 745
+			num.subtractOne(); // 744
+			num.subtractOne(); // 743
+			num.subtractOne(); // 742
+			num.subtractOne(); // 741
+			num.subtractOne(); // 740
+			num.subtractOne(); // 739
+
+			result = num.toString();
+			result.should.equal('739');
+		});
+
+		it ('should handle 10 - 1', function() {
+			num = new BigInt(10);
+			num.subtractOne();
+
+			result = num.toString();
+			result.should.equal('9');
+		});
+
+		it ('should handle 30 - 1', function() {
+			num = new BigInt(30);
+			num.subtractOne();
+
+			result = num.toString();
+			result.should.equal('29');
+		});
 	});
 
-	it ('should initalize with a number', function() {
-		result = num.toString();
+	describe('Big INTs', function() {
+		beforeEach(function() {
+			num = new BigInt(9007199254740992);
+		});
 
-		result.should.equal('746');
-	});
+		it ('should addOne', function() {
+			num.addOne();
 
-	it ('should addOne under 9', function() {
-		num.addOne();
-		result = num.toString();
+			result = num.toString();
+			result.should.equal('9007199254740993');
+		});
 
-		result.should.equal('747');
-	});
+		it ('should subtractOne', function() {
+			num.subtractOne();
 
-	it ('should addOne at 9', function() {
-		num.addOne(); 	// 7
-		num.addOne(); 	// 8
-		num.addOne(); 	// 9
-		num.addOne(); 	// 10
-
-		result = num.toString();
-		result.should.equal('750');
-	});
-
-	it ('should handle 9 + 1', function() {
-		num = new BigInt(9);
-		num.addOne();
-
-		result = num.toString();
-		result.should.equal('10');
-	});
-
-	it ('should subtractOne', function() {
-		num.subtractOne();
-
-		result = num.toString();
-		result.should.equal('745');
-	});
-
-	it ('should roll backwars', function() {
-		num.subtractOne(); // 745
-		num.subtractOne(); // 744
-		num.subtractOne(); // 743
-		num.subtractOne(); // 742
-		num.subtractOne(); // 741
-		num.subtractOne(); // 740
-		num.subtractOne(); // 739
-
-		result = num.toString();
-		result.should.equal('739');
-	});
-
-	it ('should handle 10 - 1', function() {
-		num = new BigInt(10);
-		num.subtractOne();
-
-		result = num.toString();
-		result.should.equal('9');
-	});
-
-	it ('should handle 30 - 1', function() {
-		num = new BigInt(30);
-		num.subtractOne();
-
-		result = num.toString();
-		result.should.equal('29');
+			result = num.toString();
+			result.should.equal('9007199254740991');
+		});
 	});
 });
